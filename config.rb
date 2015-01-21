0856ba2216d63654ec0cae69dd966bcbe3aa37a8
Adam Ransome <adam@core-webdesign.co.uk>
2015-01-21 12:31:17 +0000

    Commented out imageoptim activation

--- config.rb
+++ config.rb
@@ -1,11 +1,5 @@
 require 'font-awesome-sass'
 
-activate :imageoptim
-
-configure :development do
-  activate :livereload
-end
-
 compass_config do |config|
   config.add_import_path "bower_components/foundation/scss"
   config.http_path = "/"
@@ -15,22 +9,28 @@ compass_config do |config|
   config.javascripts_dir = "javascripts"
 end
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-
 helpers do
   def nav_active(page)
     current_page.path == page ? {:class => "active"} : {}
   end
 end
 
-after_configuration do
-  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
-  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# activate :imageoptim
+
+configure :development do
+  activate :livereload
 end
 
 configure :build do
   # activate :minify_css
   # activate :minify_javascript
+end
+
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
 end
